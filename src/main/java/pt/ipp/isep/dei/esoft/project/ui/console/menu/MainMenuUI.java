6a2fe5b41183be35b7931967e548db985a230bd4
Ruirm <1221696@isep.ipp.pt>
2024-05-08 09:34:07 +0100

    job and skill class tested in the mainMenuUI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/MainMenuUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/MainMenuUI.java
@@ -5,7 +5,7 @@ import pt.ipp.isep.dei.esoft.project.ui.console.authorization.AuthenticationUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 import pt.ipp.isep.dei.esoft.project.domain.Job;
 import pt.ipp.isep.dei.esoft.project.domain.Skill;
-
+import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +21,9 @@ public class MainMenuUI implements Runnable {
     public void run() {
         testJob(); // Test the Job class
         testSkill(); // Test the Skill class
+
+        printJobRepository(); // Print the content of the Job repository
+        printSkillRepository(); // Print the content of the Skill repository
 
         List<MenuItem> options = new ArrayList<MenuItem>();
         options.add(new MenuItem("Do Login", new AuthenticationUI()));
@@ -41,6 +44,7 @@ public class MainMenuUI implements Runnable {
         // Test the Job class
         String jobName = "Podador";
         Job job = new Job(jobName);
+        Repositories.getInstance().getJobRepository().add(job);
         System.out.println("Job Name: " + job.getName());
     }
 
@@ -48,6 +52,22 @@ public class MainMenuUI implements Runnable {
         // Test the Skill class
         String skillName = "Especialista de pesados";
         Skill skill = new Skill(skillName);
+        Repositories.getInstance().getSkillRepository().add(skill);
         System.out.println("Skill Name: " + skill.getName());
+    }
+    private void printJobRepository() {
+        List<Job> jobs = Repositories.getInstance().getJobRepository().getJobs();
+        System.out.println("Jobs in repository:");
+        for (Job job : jobs) {
+            System.out.println(job.getName());
+        }
+    }
+
+    private void printSkillRepository() {
+        List<Skill> skills = Repositories.getInstance().getSkillRepository().getSkills();
+        System.out.println("Skills in repository:");
+        for (Skill skill : skills) {
+            System.out.println(skill.getName());
+        }
     }
 }
