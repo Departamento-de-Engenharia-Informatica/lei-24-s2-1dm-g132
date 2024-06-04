3f62fa259652e830df4b1b90bf672223e16a5835
jos3-silva <148633470+jos3-silva@users.noreply.github.com>
2024-06-04 12:24:49 +0100

    US21 and 22 validation changes in GSTask class

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/GSTask.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/GSTask.java
@@ -12,24 +12,32 @@ public class GSTask implements Serializable {
 
     private String description;
 
-    private String degreeOfUrgency;
+    private DegreeOfUrgency degreeOfUrgency;
 
     private int expectedDuration;
 
     private GreenSpace greenSpace;
 
-    private String status;
+    private GSTaskStatus status;
 
     private Calendar startingDate;
 
+    private enum DegreeOfUrgency {
+        Low, Medium, High
+    }
+
+    private enum GSTaskStatus {
+        Pending, Processed, Planned, Postponed, Canceled, Done
+    }
+
     public GSTask(String title, String description, String degreeOfUrgency, int expectedDuration, GreenSpace selectedGreenSpace)
     {
-        this.title = title;
+        setTitle(title);
         this.description = description;
-        this.degreeOfUrgency = degreeOfUrgency;
-        this.expectedDuration = expectedDuration;
+        setDegreeOfUrgency(degreeOfUrgency);
+        setExpectedDuration(expectedDuration);
         this.greenSpace = selectedGreenSpace;
-        this.status = "Pending";
+        this.status = GSTaskStatus.Pending;
     }
 
     public String getTitle() {
@@ -41,7 +49,7 @@ public class GSTask implements Serializable {
     }
 
     public String getDegreeOfUrgency() {
-        return degreeOfUrgency;
+        return degreeOfUrgency.toString();
     }
 
     public int getExpectedDuration() {
@@ -52,14 +60,43 @@ public class GSTask implements Serializable {
         return greenSpace;
     }
 
+    private void setTitle(String title)
+    {
+        if (title.matches("[a-zA-Z ]*" )) {
+            this.title = title.trim();
+        } else {
+            throw new IllegalArgumentException("Title cannot contain special characters.");
+        }
+    }
+
+    private void setDegreeOfUrgency(String degreeOfUrgency)
+    {
+        if(degreeOfUrgency.equalsIgnoreCase(DegreeOfUrgency.Low.toString()))
+            this.degreeOfUrgency = DegreeOfUrgency.Low;
+        else if(degreeOfUrgency.equalsIgnoreCase(DegreeOfUrgency.Medium.toString()))
+            this.degreeOfUrgency = DegreeOfUrgency.Medium;
+        else if(degreeOfUrgency.equalsIgnoreCase(DegreeOfUrgency.High.toString()))
+            this.degreeOfUrgency = DegreeOfUrgency.High;
+        else throw new IllegalArgumentException("Invalid degree of urgency.");
+    }
+
+    private void setExpectedDuration(int expectedDuration)
+    {
+        if (expectedDuration > 0) {
+            this.expectedDuration = expectedDuration;
+        } else {
+            throw new IllegalArgumentException("Expected duration must be a positive integer.");
+        }
+    }
+
     public void setProcessed()
     {
-        this.status = "Processed";
+        this.status = GSTaskStatus.Processed;
     }
 
     public void plan(String startingDate)
     {
-        this.status = "Planned";
+        this.status = GSTaskStatus.Planned;
         setStartingDate(startingDate);
     }
 
@@ -85,12 +122,19 @@ public class GSTask implements Serializable {
         cal.set(Calendar.MONTH, month - 1);
         cal.set(Calendar.DAY_OF_MONTH, day);
 
+        Calendar currentDate = Calendar.getInstance();
+
+        if(cal.before(currentDate))
+        {
+            throw new IllegalArgumentException("Task starting date must be after the current date.");
+        }
+
         this.startingDate = cal;
     }
 
     public boolean isPending()
     {
-        return this.status.equalsIgnoreCase("Pending");
+        return this.status.equals(GSTaskStatus.Pending);
     }
 
     public boolean hasUserEmail(String email)
@@ -101,7 +145,7 @@ public class GSTask implements Serializable {
     @Override
     public GSTask clone()
     {
-        return new GSTask(this.title, this.description, this.degreeOfUrgency, this.expectedDuration, this.greenSpace);
+        return new GSTask(this.title, this.description, this.degreeOfUrgency.toString(), this.expectedDuration, this.greenSpace);
     }
 
 }
