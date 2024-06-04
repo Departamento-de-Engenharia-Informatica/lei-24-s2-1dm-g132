a71335c1982afb4432563edbe78c078238ca7601
jos3-silva <148633470+jos3-silva@users.noreply.github.com>
2024-06-04 18:59:39 +0100

    US23 checkTeamSchedule method logic implementation

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/Agenda.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/Agenda.java
@@ -73,16 +73,71 @@ public class Agenda implements Serializable {
                 teamSchedule.add(gSTask);
             }
         }
+        //Simplificação: se for uma task de mais de um dia, só pode começar num dia livre
 
-        /*verification of the team schedule
+        int workDayHours = 8;
+        int availableDayHours = 8;
+        int minDays = selectedTask.getExpectedDuration() / workDayHours;
+        int totalOfRequestedHours = selectedTask.getExpectedDuration();
+
         for(GSTask gSTask : teamSchedule)
         {
-            int numberOfDays = gSTask.getExpectedDuration() / 8;
+            if(minDays == 0)                                                                                                    //Quando a task selecionada pode ser feita dentro de um dia
+            {
+                if(gSTask.getStartingDate().equals(selectedTask.getStartingDate()))                                             //Tirar o tempo das tasks que também começam no mesmo dia
+                {
+                    availableDayHours -= gSTask.getExpectedDuration();
+                }
+                else if(gSTask.getStartingDate().before(selectedTask.getStartingDate()) && gSTask.getExpectedDuration() > 8)    //Caso a task inicie antes mas tenha mais que um dia
+                {
+                    Calendar endingDate = (Calendar) gSTask.getStartingDate().clone();
+                    endingDate.add(Calendar.DAY_OF_MONTH, gSTask.getExpectedDuration() / workDayHours);
+                    if(endingDate.equals(selectedTask.getStartingDate()))                                                       //Task termina no mesmo dia que a selecionada começa
+                    {
+                        availableDayHours -= gSTask.getExpectedDuration() % workDayHours;
+                    }
+                    else if (endingDate.after(selectedTask.getStartingDate()))                                                  //Task termina só depois da selecionada começar (não pode)
+                        return false;
+                }
+            }
+            else                                                                                                                //Quando a task selecionada necessita de mais de um dia
+            {
+                if(gSTask.getStartingDate().equals(selectedTask.getStartingDate()))                                             //Simplificação da task só poder começar num dia sem tasks anteriormente
+                {
+                    return false;
+                }
+                else if(gSTask.getStartingDate().before(selectedTask.getStartingDate()) && gSTask.getExpectedDuration() > workDayHours)     //Quando a task começa antes mas tem mais que um dia
+                {
+                    Calendar endingDate = (Calendar) gSTask.getStartingDate().clone();
+                    endingDate.add(Calendar.DAY_OF_MONTH, gSTask.getExpectedDuration() / workDayHours);
+                    if(endingDate.equals(selectedTask.getStartingDate()) || endingDate.after(selectedTask.getStartingDate()))               //O dia não pode já ter parte de outra task ou a outra task não pode terminar depois do começo da selecionada
+                    {
+                        return false;
+                    }
+                }
+                else if(gSTask.getStartingDate().after(selectedTask.getStartingDate()))                                                     //Caso a task seja depois da selecionada
+                {
+                    Calendar endingDateSelectedTask = (Calendar) selectedTask.getStartingDate().clone();
+                    endingDateSelectedTask.add(Calendar.DAY_OF_MONTH, selectedTask.getExpectedDuration() / workDayHours);
+                    if(endingDateSelectedTask.after(gSTask.getStartingDate()))                                                              //Se a task selecionada só terminar depois do começo desta
+                    {
+                        return false;
+                    }
+                    else if(endingDateSelectedTask.equals(gSTask.getStartingDate()))                                                        //Se a selecionada terminar no mesmo dia que esta começa
+                    {
+                        if(gSTask.getExpectedDuration() >= workDayHours)                                                                    //Se esta for maior que um dia de trabalho
+                            return false;
+                        else                                                                                                                //Se der para terminar a selecionada dias e terminar esta
+                            availableDayHours -= gSTask.getExpectedDuration() % 8;
+                    }
+                }
+            }
+        }
 
-
-        }*/
-
-        return true;
+        if(availableDayHours < totalOfRequestedHours % 8)
+            return false;
+        else
+            return true;
     }
 
     public Optional<GSTask> assignTeam(GSTask selectedTask, Team selectedTeam)
