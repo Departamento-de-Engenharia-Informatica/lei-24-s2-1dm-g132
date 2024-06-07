7d3d47c94a3f8a25d4a67ab380db5fdb2d64a052
BiaMorais30 <127674486+biamorais3@users.noreply.github.com>
2024-06-07 15:57:29 +0100

    changed name of menu items

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/gsm/GSMMenuUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/gsm/GSMMenuUI.java
@@ -14,10 +14,10 @@ public class GSMMenuUI implements Runnable{
     @Override
     public void run() {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Import a csv file", new ImportWaterSupplyPointsCsvUI()));
-        options.add(new MenuItem("Apply an algorithm that returns the routes to be opened and pipes needed to be laid with a minimum accumulated cost", new MinimalAcumulatedCostGraphUI()));
-        options.add(new MenuItem("Place signs to evacuate park users to an Assembly Point", new ImportMeetingPointsCsvUI()));
-        options.add(new MenuItem("Place signs to evacuate park users to one of the several Assembly Points", new ShortestPathsToMeetingPointUI()));
+        options.add(new MenuItem("Import csv file with water supply", new ImportWaterSupplyPointsCsvUI()));
+        options.add(new MenuItem("Calculate minimum accumulated cost", new MinimalAcumulatedCostGraphUI()));
+        options.add(new MenuItem("Import csv file with meeting points", new ImportMeetingPointsCsvUI()));
+        options.add(new MenuItem("Place signs to evacuate park users to an assembly point and to one of the several meeting points", new ShortestPathsToMeetingPointUI()));
 
 
         int option = 0;
