bc39cb0bc0118bc21b8fc7a1485631530eeb38cc
jos3-silva <148633470+jos3-silva@users.noreply.github.com>
2024-06-08 16:37:03 +0100

    Serialization classes javadoc comments addition

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/serialization/GreenSpaceRepositoryFile.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/serialization/GreenSpaceRepositoryFile.java
@@ -4,20 +4,49 @@ import pt.ipp.isep.dei.esoft.project.repository.GreenSpaceRepository;
 
 import java.io.*;
 
+
+/**
+ * Class responsible for reading and saving {@link GreenSpaceRepository} objects to and from a file.
+ * The class provides methods to read and save the repository data using serialization.
+ */
 public class GreenSpaceRepositoryFile {
+
+    /**
+     * The default file name used for storing the green space repository.
+     */
     public static final String FILE_NAME = "GreenSpaceRepository.ltf";
 
+    /**
+     * Constructs a new GreenSpaceRepositoryFile object.
+     */
     public GreenSpaceRepositoryFile() {
     }
 
+    /**
+     * Reads the green space repository from the default file.
+     *
+     * @return The {@link GreenSpaceRepository} object read from the file. If the file does not exist or an error occurs, a new empty {@link GreenSpaceRepository} is returned.
+     */
     public GreenSpaceRepository read() {
         return read(FILE_NAME);
     }
 
+    /**
+     * Reads the green space repository from a specified file name.
+     *
+     * @param fileName The name of the file to read the green space repository from.
+     * @return The {@link GreenSpaceRepository} object read from the file. If the file does not exist or an error occurs, a new empty {@link GreenSpaceRepository} is returned.
+     */
     public GreenSpaceRepository read(String fileName) {
         return read(new File(fileName));
     }
 
+    /**
+     * Reads the green space repository from a specified file.
+     *
+     * @param file The file to read the green space repository from.
+     * @return The {@link GreenSpaceRepository} object read from the file. If the file does not exist or an error occurs, a new empty {@link GreenSpaceRepository} is returned.
+     */
     public GreenSpaceRepository read(File file) {
         GreenSpaceRepository greenSpaceRepository;
         try {
@@ -34,14 +63,34 @@ public class GreenSpaceRepositoryFile {
         }
     }
 
+    /**
+     * Saves the green space repository to the default file.
+     *
+     * @param greenSpaceRepository The {@link GreenSpaceRepository} object to save.
+     * @return {@code true} if the green space repository was successfully saved; {@code false} otherwise.
+     */
     public boolean save(GreenSpaceRepository greenSpaceRepository) {
         return save(FILE_NAME, greenSpaceRepository);
     }
 
+    /**
+     * Saves the green space repository to a specified file name.
+     *
+     * @param fileName The name of the file to save the green space repository to.
+     * @param greenSpaceRepository The {@link GreenSpaceRepository} object to save.
+     * @return {@code true} if the green space repository was successfully saved; {@code false} otherwise.
+     */
     public boolean save(String fileName, GreenSpaceRepository greenSpaceRepository) {
         return save(new File(fileName), greenSpaceRepository);
     }
 
+    /**
+     * Saves the green space repository to a specified file.
+     *
+     * @param file The file to save the green space repository to.
+     * @param greenSpaceRepository The {@link GreenSpaceRepository} object to save.
+     * @return {@code true} if the green space repository was successfully saved; {@code false} otherwise.
+     */
     public boolean save(File file, GreenSpaceRepository greenSpaceRepository) {
         try {
             ObjectOutputStream out = new ObjectOutputStream(
--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/serialization/ToDoListFile.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/serialization/ToDoListFile.java
@@ -4,20 +4,48 @@ import pt.ipp.isep.dei.esoft.project.repository.ToDoList;
 
 import java.io.*;
 
+/**
+ * Class responsible for reading and saving {@link ToDoList} objects to and from a file.
+ * The class provides methods to read and save the to-do list data using serialization.
+ */
 public class ToDoListFile {
+
+    /**
+     * The default file name used for storing the to-do list.
+     */
     public static final String FILE_NAME = "ToDoList.ltf";
 
+    /**
+     * Constructs a new ToDoListFile object.
+     */
     public ToDoListFile() {
     }
 
+    /**
+     * Reads the to-do list from the default file.
+     *
+     * @return The {@link ToDoList} object read from the file. If the file does not exist or an error occurs, a new empty {@link ToDoList} is returned.
+     */
     public ToDoList read() {
         return read(FILE_NAME);
     }
 
+    /**
+     * Reads the to-do list from a specified file name.
+     *
+     * @param fileName The name of the file to read the to-do list from.
+     * @return The {@link ToDoList} object read from the file. If the file does not exist or an error occurs, a new empty {@link ToDoList} is returned.
+     */
     public ToDoList read(String fileName) {
         return read(new File(fileName));
     }
 
+    /**
+     * Reads the to-do list from a specified file.
+     *
+     * @param file The file to read the to-do list from.
+     * @return The {@link ToDoList} object read from the file. If the file does not exist or an error occurs, a new empty {@link ToDoList} is returned.
+     */
     public ToDoList read(File file) {
         ToDoList toDoList;
         try {
@@ -34,14 +62,34 @@ public class ToDoListFile {
         }
     }
 
+    /**
+     * Saves the to-do list to the default file.
+     *
+     * @param toDoList The {@link ToDoList} object to save.
+     * @return {@code true} if the to-do list was successfully saved; {@code false} otherwise.
+     */
     public boolean save(ToDoList toDoList) {
         return save(FILE_NAME, toDoList);
     }
 
+    /**
+     * Saves the to-do list to a specified file name.
+     *
+     * @param fileName The name of the file to save the to-do list to.
+     * @param toDoList The {@link ToDoList} object to save.
+     * @return {@code true} if the to-do list was successfully saved; {@code false} otherwise.
+     */
     public boolean save(String fileName, ToDoList toDoList) {
         return save(new File(fileName), toDoList);
     }
 
+    /**
+     * Saves the to-do list to a specified file.
+     *
+     * @param file The file to save the to-do list to.
+     * @param toDoList The {@link ToDoList} object to save.
+     * @return {@code true} if the to-do list was successfully saved; {@code false} otherwise.
+     */
     public boolean save(File file, ToDoList toDoList) {
         try {
             ObjectOutputStream out = new ObjectOutputStream(
